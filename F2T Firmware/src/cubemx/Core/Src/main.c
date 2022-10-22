3d904c14fde2ed034c230b180924c8601d128cc1
LeoDJ <leandro.spaeth@gmail.com>
2022-10-22 22:54:46 +0200

    implement USB serial

--- F2T Firmware/src/cubemx/Core/Src/main.c
+++ F2T Firmware/src/cubemx/Core/Src/main.c
@@ -20,6 +20,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "usart.h"
+#include "usb_device.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -76,9 +77,8 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -88,17 +88,20 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_USART2;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB|RCC_PERIPHCLK_USART1
+                              |RCC_PERIPHCLK_USART2;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
   PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
--- F2T Firmware/src/main.cpp
+++ F2T Firmware/src/main.cpp
@@ -2,10 +2,29 @@
 
 #include "main.h"
 #include "gpio.h"
+#include "usb_device.h"
+#include "usbd_cdc_if.h"
 #include "usart.h"
+
+#include <errno.h>
+#include <sys/unistd.h>
 
 extern "C" {
     void SystemClock_Config(void);
+
+    // enable printf functionality on PRINTF_UART
+    int _write(int file, char *data, int len) {
+        if ((file != STDOUT_FILENO) && (file != STDERR_FILENO)) {
+            errno = EBADF;
+            return -1;
+        }
+
+        // HAL_StatusTypeDef status = HAL_UART_Transmit(&PRINTF_UART, (uint8_t *)data, len, 1000);
+        // return (status == HAL_OK ? len : 0);
+        // HAL_GPIO_TogglePin(LED_2_B_GPIO_Port, LED_2_B_Pin);
+        uint8_t statusU = CDC_Transmit_FS((uint8_t *)data, len);
+        return (statusU == USBD_OK ? len : 0);
+    }
 }
 
 int main(void) {
@@ -17,9 +36,11 @@ int main(void) {
     MX_USART2_UART_Init();
     MX_USART3_UART_Init();
     MX_USART4_UART_Init();
+    MX_USB_DEVICE_Init();
 
     while (1) {
-        
+        HAL_Delay(500);
+        printf("Hello World!\n");
     }
 }
 
